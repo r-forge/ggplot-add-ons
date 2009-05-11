56ac65585940bfee7f0878ac4585561730a9750a
baptiste (baptiste auguie) <baptiste.auguie@gmail.com>
2009-05-11 18:50:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ggplot-add-ons@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/scale-sides.r
@@ -0,0 +1,30 @@
+
+ScaleSides <- proto(ScaleDiscrete, expr={
+  doc <- TRUE
+  common <- NULL
+  .input <- .output <- "sides"
+ 
+  output_set <- function(.) c(3, 4, 5, 6, 7, 8, 50)[seq_along(.$input_set())]
+  max_levels <- function(.) 9
+  
+  detail <- "<p></p>"
+  
+  # Documentation -----------------------------------------------
+ 
+  objname <- "sides"
+  desc <- "Scale for polygon sides"
+  
+  icon <- function(.) {
+  # 
+  }
+  
+  examples <- function() {
+    #  
+    # See scale_manual for more flexibility
+  }
+  
+})
+
+
+scale_sides <- ScaleSides$build_accessor()
+
