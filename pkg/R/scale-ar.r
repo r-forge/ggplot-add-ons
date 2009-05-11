2caef2cff9b9d33d7ae86dc2536786fde04b289a
baptiste (baptiste auguie) <baptiste.auguie@gmail.com>
2009-05-11 19:19:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ggplot-add-ons@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/scale-ar.r
@@ -0,0 +1,34 @@
+ScaleAr <- proto(ScaleContinuous, expr={
+  doc <- TRUE
+  common <- NULL
+  
+  new <- function(., name=NULL, limits=NULL, breaks=NULL, labels=NULL, trans = NULL, to = c(1, 5)) {
+    .super$new(., name=name, limits=limits, breaks=breaks, labels=labels, trans=trans, variable = "ar", to = to)
+  }
+  
+  map <- function(., values) {
+    rescale(values, .$to, .$input_set())
+  }
+  output_breaks <- function(.) .$map(.$input_breaks())
+  
+  objname <- "ar"
+  desc <- "Aspect ratio scale for continuous variable"
+  
+  icon <- function(.) {
+    x <- c(0.1, 0.3, 0.5, 0.7, 0.9)
+    rectGrob(x, width=0.25,
+      gp=gpar(fill=alpha("black", x), col=NA)
+    )
+    
+  }
+  
+  examples <- function(.) {
+    (p <- qplot(mpg, cyl, data=mtcars, alpha=cyl))
+    p + scale_alpha("cylinders")
+    p + scale_alpha("number\nof\ncylinders")
+    
+    p + scale_alpha(to = c(0.4, 0.8))
+  }
+})
+
+scale_ar <- ScaleAr$build_accessor()
