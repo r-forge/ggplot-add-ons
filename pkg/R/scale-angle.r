7d2e43a2385a1a665f15e3deb9a4041c5bd35423
baptiste (baptiste auguie) <baptiste.auguie@gmail.com>
2009-05-12 11:04:47 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ggplot-add-ons@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/scale-angle.r
+++ pkg/R/scale-angle.r
@@ -1,31 +1,34 @@
-ScaleAngle <- proto(ScaleContinuous, expr={
-	
-  new <- function(., name=NULL, limits=NULL, breaks=NULL, labels=NULL, trans = NULL, to = c(0, 2*pi)) {
+	ScaleAngle <- proto(ScaleContinuous, expr={
+  doc <- TRUE
+  common <- NULL
+  
+  new <- function(., name=NULL, limits=NULL, breaks=NULL, labels=NULL, trans = NULL, to = c(0, pi)) {
     .super$new(., name=name, limits=limits, breaks=breaks, labels=labels, trans=trans, variable = "angle", to = to)
   }
-
+  
   map <- function(., values) {
     rescale(values, .$to, .$input_set())
   }
   output_breaks <- function(.) .$map(.$input_breaks())
   
-  
-
-  # Documentation -----------------------------------------------
-  
   objname <- "angle"
-  desc <- "angle of rotation of a ngon"
+  desc <- "Alpha scale for continuous variable"
+  
   icon <- function(.) {
-    # 
+    x <- c(0.1, 0.3, 0.5, 0.7, 0.9)
+    rectGrob(x, width=0.25,
+      gp=gpar(fill=alpha("black", x), col=NA)
+    )
+    
   }
- 
- 
+  
   examples <- function(.) {
-  # 
+    (p <- qplot(mpg, cyl, data=mtcars, alpha=cyl))
+    p + scale_alpha("cylinders")
+    p + scale_alpha("number\nof\ncylinders")
+    
+    p + scale_alpha(to = c(0.4, 0.8))
   }
-  
-  
 })
-
 
 scale_angle <- ScaleAngle$build_accessor()
