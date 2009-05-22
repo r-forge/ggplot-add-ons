cc5a291406eb4482b255cfe8a7b40496b462740f
baptiste (baptiste auguie) <baptiste.auguie@gmail.com>
2009-05-22 20:45:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ggplot-add-ons@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.r
+++ pkg/R/zzz.r
@@ -8,12 +8,21 @@ scale_angle_discrete <- ScaleAngleDiscrete$build_accessor()
 scale_ar <- ScaleAr$build_accessor() # default is continuous
 scale_ar_discrete <- ScaleArDiscrete$build_accessor()
 
+scale_length <- ScaleLength$build_accessor() # default is continuous
+
 geom_ngon <- GeomNgon$build_accessor()
 geom_star <- GeomStar$build_accessor()
 geom_ellipse <- GeomEllipse$build_accessor()
+geom_field <- GeomField$build_accessor()
 # geom_custom <- GeomCustom$build_accessor() # not sure how to make this one work
 
 
 scale_angle_manual <- ScaleManual$build_accessor(list(variable = "\"angle\""))
 scale_ar_manual <- ScaleManual$build_accessor(list(variable = "\"ar\""))
 scale_sides_manual <- ScaleManual$build_accessor(list(variable = "\"sides\""))
+scale_length_manual <- ScaleManual$build_accessor(list(variable = "\"length\""))
+
+scale_alpha_manual <- ScaleManual$build_accessor(list(variable = "\"alpha\""))
+
+
+scale_colour_dichromat <- ScaleDichromat$build_accessor(list(variable = "\"colour\""))
